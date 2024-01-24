e200dec25ee72416081e4b07e7ca2b43eb47fc33
Iurii <virgomessier104@protonmail.com>
2024-01-25 03:49:02 +0400

    V1 Start of practice

---
+++ js/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// const data = parseInt('214fd61b', 16);
+
+// const key = parseInt('212ad5ff', 16);
+
+
+
+// const shifr = data ^ key;
+// const back = 6620132 ^ key;
+// const back16 = back.toString(16);
+// console.log(back.toString(16));
+
+
+const checkIt = true, checkOn = false;
+
+console.log(checkIt && checkOn);
