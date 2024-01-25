773ca922ae63514ced5de45eadcbb764bc07ceb4
Iurii <virgomessier104@protonmail.com>
2024-01-25 04:35:14 +0400

    V1 PTask Number 1

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
 'use strict';
 
-// const data = parseInt('214fd61b', 16);
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// const key = parseInt('212ad5ff', 16);
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
 
+const latestFilm = prompt('Один из последних просмотренных фильмов', ''), 
+	rating = prompt('На сколько оцените его?', ''), 
+	favoriteFilm = prompt('Любимый фильм', ''), 
+	favoriteRating = prompt('Его оценка', '');
 
+personalMovieDB.movies[latestFilm] = rating;
+personalMovieDB.movies[favoriteFilm] = favoriteRating;
 
-// const shifr = data ^ key;
-// const back = 6620132 ^ key;
-// const back16 = back.toString(16);
-// console.log(back.toString(16));
-
-
-const checkIt = true, checkOn = false;
-
-console.log(checkIt && checkOn);
+console.log(personalMovieDB);
