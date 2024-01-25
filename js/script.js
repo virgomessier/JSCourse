7a66ecebce9b09af92b316edd1b5915ad7f88074
Iurii <virgomessier104@protonmail.com>
2024-01-26 00:44:39 +0400

    V1 PTask Number 3 (logic gates: && ||)

--- js/script.js
+++ js/script.js
@@ -1,41 +1,81 @@
 'use strict';
 
-if (1) {
-	console.log('okay');
-} else  {
-	console.log('error');
-}
-
-
-const num1 = 50;
-
-if (num1 <= 49 ) {
-	console.log('error');
-} else if (num1 > 100) {
-	console.log('слишком много');
+const hamburger = 5;
+const fries = null;
+if (hamburger && fries) {
+	console.log('Cет: гамбурег и картошка');
 } else {
-	console.log('Отличная сумма');
+	console.log('Добавьте еще позицию для сет набора');
+}
+
+const hamburger = 3;
+const fries = 1;
+const cola = 1;
+console.log(hamburger >= 3 && cola && fries);
+if (hamburger >= 3 && cola && fries) {
+	console.log('Cет: 3+ гамбургера кола и картошка фри');
+} else {
+	console.log('Добавьте еще позицию для набора');
+}
+
+console.log(0 && 1);
+console.log(1 && 5);
+console.log(null && 5);
+console.log(1 && 'случайная строка');
+
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+	console.log('Все довольны');
+} else {
+	console.log('Мы уходим');
+}
+console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
+
+
+
+let johnReport, alexReport, samReport, mariaReport = 'done';
+console.log(johnReport || alexReport || samReport || mariaReport);
+
+
+
+console.log( NaN || 2 || undefined );
+console.log( NaN && 2 && undefined );
+console.log( 1 && 2 && 3 );
+console.log( !1 && 2 || !3 );
+console.log( 25 || null && !3 );
+console.log( NaN || null || !3 || undefined || 5);
+console.log( NaN || null && !3 && undefined || 5);
+console.log( 5 === 5 && 3 > 1 || 5);
+
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+if (hamburger === 3 && cola || fries === 3 && nuggets) {
+   console.log('Done!')
 }
 
 
+// Переменная hamburger задана через let, так как const подразумевает какое-либо начальное значение. С const будет ошибка, можете проверить.
+// А без значения там undefined, напоминаю.
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+if (hamburger || cola || fries === 3 || nuggets) {
+   console.log('Done!')
+};
 
-(num1 === 50) ? console.log('Отличная сумма') : console.log('error');
 
-
-
-const num3 = 51;
-
-switch (num3) {
-case 49:
-	console.log('неверно');
-	break;		
-case 100:
-	console.log('неверно');
-	break;
-case 50:
-	console.log('В точку');
-	break;
-default:
-	console.log('не в этот раз');
-	break;
-}
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+};
