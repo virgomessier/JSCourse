1c3ba5acf88d5199019b33063877406af9e0144d
Iurii <virgomessier104@protonmail.com>
2024-01-26 16:01:47 +0400

    V1 PTask Number 4 (cycles: while,do,for)

--- js/script.js
+++ js/script.js
@@ -1,81 +1,29 @@
 'use strict';
 
-const hamburger = 5;
-const fries = null;
-if (hamburger && fries) {
-	console.log('Cет: гамбурег и картошка');
-} else {
-	console.log('Добавьте еще позицию для сет набора');
+let num = 50;
+
+while (num <= 55) {
+	console.log(num);
+	num++;
 }
 
-const hamburger = 3;
-const fries = 1;
-const cola = 1;
-console.log(hamburger >= 3 && cola && fries);
-if (hamburger >= 3 && cola && fries) {
-	console.log('Cет: 3+ гамбургера кола и картошка фри');
-} else {
-	console.log('Добавьте еще позицию для набора');
+do {
+	console.log(num);
+	num++;
 }
-
-console.log(0 && 1);
-console.log(1 && 5);
-console.log(null && 5);
-console.log(1 && 'случайная строка');
+while (num < 55);
 
 
-const hamburger = 3;
-const fries = 3;
-const cola = 0;
-const nuggets = 2;
-if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
-	console.log('Все довольны');
-} else {
-	console.log('Мы уходим');
-}
-console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
-
-
-
-let johnReport, alexReport, samReport, mariaReport = 'done';
-console.log(johnReport || alexReport || samReport || mariaReport);
-
-
-
-console.log( NaN || 2 || undefined );
-console.log( NaN && 2 && undefined );
-console.log( 1 && 2 && 3 );
-console.log( !1 && 2 || !3 );
-console.log( 25 || null && !3 );
-console.log( NaN || null || !3 || undefined || 5);
-console.log( NaN || null && !3 && undefined || 5);
-console.log( 5 === 5 && 3 > 1 || 5);
-
-
-const hamburger = 3;
-const fries = 3;
-const cola = 0;
-const nuggets = 2;
-if (hamburger === 3 && cola || fries === 3 && nuggets) {
-   console.log('Done!')
+for (let i = 1; i < 10; i++) {
+	console.log(num);
+	num++;	
 }
 
 
-// Переменная hamburger задана через let, так как const подразумевает какое-либо начальное значение. С const будет ошибка, можете проверить.
-// А без значения там undefined, напоминаю.
-let hamburger;
-const fries = NaN;
-const cola = 0;
-const nuggets = 2;
-if (hamburger || cola || fries === 3 || nuggets) {
-   console.log('Done!')
-};
-
-
-let hamburger;
-const fries = NaN;
-const cola = 0;
-const nuggets = 2;
-if (hamburger && cola || fries === 3 && nuggets) {
-   console.log('Done!')
-};
+for (let i = 1; i < 10; i++) {
+	if (i === 6) {
+		break;
+		// continue;
+	}
+	console.log(i);
+}
