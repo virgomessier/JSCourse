c47b06e02a1eb8f5e569c70ab285932bdadc5c44
Iurii <virgomessier104@protonmail.com>
2024-01-28 00:16:26 +0400

    V1 PTask Number 6 (cycles: for - practice 2)

--- js/script.js
+++ js/script.js
@@ -1,103 +1,51 @@
 'use strict';
 
-for (let i = 0; i < 3; i++) {
-	console.log(i);
-	for (let j = 0; j < 3; j++) {
-		console.log(j);
-		
-	}
+
+const arr = [3, 5, 8, 16, 20, 23, 50];
+const result = [];
+
+for (let i = 0; i < arr.length; i++) {
+	result[i] = arr[i];
 }
 
-let result = '';
-const lenght = 7;
-
-for (let i = 1; i < lenght; i++) {
-	for (let j = 0; j < i; j++) {
-		result += '*';
-	}
-	result += '\n';
-}
 console.log(result);
 
 
-first: for (let i = 0; i < 3; i++) {
-	console.log(`First level: ${i}`);
-	for (let j = 0; j < 3; j++) {
-		console.log(`Second level: ${j}`);
-		for (let k = 0; k < 3; k++) {
-			if (k === 2) continue first;
-			console.log(`Third level: ${k}`);
-			
-		}
-		
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+for (let i = 0; i < data.length; i++) {
+	if (typeof(data[i]) === 'number') {
+		data[i] = data[i] * 2;
+	}
+	if (typeof(data[i]) === 'string') {
+		data[i] = `${data[i]} - done`;
 	}
 }
+console.log(data);
 
 
-for (let i = 5; i < 11; i++) {
-	console.log(i);
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+const result = [];
+for (let i = 1; i <= data.length; i++) {
+	result[i - 1] = data[data.length - i];
 }
 
-firstCycle: for (let i = 20; i > 9; i--) {
-	if (i === 13) break firstCycle;
-	console.log(i);
-}
+console.log(result);
 
-// firstCycle: for (let i = 2; i < 11; i++) {
-// 	console.log(i); 
-// 	for (let j = 4; j < 11; j++) {
-// 		console.log(j);
-// 		for (let k = 6; k < 11; k++) {
-// 			console.log(k);
-// 			for (let l = 8; l < 11; l++) {
-// 				console.log(l);
-// 				for (let m = 10; l < 11; m++) {
-// 					console.log(m);
-// 					if (m === 10) break firstCycle;
-// 				}
-// 			}
-// 		}
-		
-// 	}
-// }
 
-// вывод в консоль четных чисел
-for (let i = 2; i <= 10; i++) {
-	if (i % 2 === 1) {
-		continue;
-	} else {
-		console.log(i);
+// задачка на елочку из '*'
+const lines = 20;
+let result = '';
+
+for (let i = 0; i <= lines; i++) {
+	for (let j = 0; j < lines - i; j++) {
+		result += ' ';
 	}
-}
-
-// Вывод в консоль нечетных чисел
-for (let i = 2; i <= 16; i++) {
-	if (i % 2 === 0) {
-		continue;
-	} else {
-		console.log(i);
+	for (let j = 0; j < 2 * i + 1; j++) {
+		result += "*";
 	}
+	result += "\n";
 }
 
-let i = 2;
-while (i < 16 ) {
-	i++;
-	if (i % 2 === 0) {
-		continue;
-	} else {
-		console.log(i);
-	}
-}
-
-
-
-const arrayOfNumbers = [];
-
-for (let i = 5; i < 11; i++) {
-	arrayOfNumbers[i - 5] = i;
-}
-
-console.log(arrayOfNumbers);
-
-
-
+console.log(result)
